2ab57205c497fc7a98948915f3d1854e09a20653
ALIREDA5 <alireda753951@gmail.com>
2024-01-02 02:54:49 +0200

    problem solving

---
+++ Bascis.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : Bascis.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include "string.h"
+using namespace std;
+
+int main() {
+
+
+
+
+	return 0;
+}
